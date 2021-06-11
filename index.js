d3c9b3246448ffb27fe5464b9e7713df5e1170cc
Angela <angelalamluu@gmail.com>
2021-06-10 22:53:09 -0700

    commit tree ed26366c733719591806ee4b6862b99b01a2cf17

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your functions here
+function map(arr, callback) {
+    let newArr = []
+
+    for (let i = 0; i < arr.length; i++) {
+        newArr.push(callback(arr[i]))
+    }
+    return newArr
+}
+
+function reduce(src, callback, startingValue) {
+    let total;
+    if (startingValue) {
+      total = startingValue;
+      for (let i = 0; i < src.length; i++) {
+        total = callback(src[i], total);
+      }
+      return total;
+    } else {
+      total = src[0];
+      for (let i = 1; i < src.length; i++) {
+        total = callback(src[i], total);
+      }
+      return total;
+    }
+  }
+    
